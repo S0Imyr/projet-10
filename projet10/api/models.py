27febd907ca86102f9b8447de34aade5954740dc
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-04-15 22:49:36 +0200

    define models and migrations

--- projet10/api/models.py
+++ projet10/api/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+CHOICES = ((1,'not allowed'), (2,'allowed'))
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=8192)
+    type = models.CharField(max_length=128)
+    author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
+
+
+class Contributor(models.Model):
+    user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    project_id = models.ForeignKey(to=Project, on_delete=models.CASCADE)
+    persmission = models.CharField(max_length=128, choices=CHOICES)
+    role = models.CharField(max_length=128)
+
+
+class Issue(models.Model):
+    title = models.CharField(max_length=128)
+    desc = models.CharField(max_length=8192)
+    tag = models.CharField(max_length=10)
+    priority = models.CharField(max_length=128)
+    project_id = models.ForeignKey(to=Project, on_delete=models.CASCADE)
+    status = models.CharField(max_length=128)
+    author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL, related_name="Written_by")
+    assignee_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL, related_name="Assign_to")
+    created_time = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    description = models.CharField(max_length=8192)
+    author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
+    issue_id = models.ForeignKey(to=Issue, on_delete=models.CASCADE)
+    created_time = models.DateTimeField(auto_now_add=True)
--- projet10/projet10/settings.py
+++ projet10/projet10/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'authentication.apps.AuthenticationConfig',
     'api.apps.ApiConfig',
 
     'rest_framework',
@@ -127,3 +128,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "authentication.user"
