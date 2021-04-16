0e646252dbe8b3bdb20c7e465022a431211cc099
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-04-16 14:00:23 +0200

    Init views function

--- projet10/api/urls.py
+++ projet10/api/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.apiOverview, name='api-overview'),
-    path('projects/', views.project_list, name='project-list'),
+    path('projects/', views.get_projects_list, name='project-list'),
 ]
--- projet10/api/views.py
+++ projet10/api/views.py
@@ -7,31 +7,123 @@ from rest_framework.response import Response
 from.serializers import ProjectSerializer
 from .models import Project
 
+
 @api_view(['GET'])
 def apiOverview(request):
     api_urls = {
         'Projects list (GET)': '/projects/',
         'Create project (POST)': '/projects/',
-        'Get the details of project (GET)': '/projects/<str:pk>',
+        'Get project details  (GET)': '/projects/<str:pk>',
         'Update a project (PUT)': '/projects/<str:pk>',
         'Delete a project (DELETE)': '/projects/<str:pk>',
         'Add a user to a project (POST)': '/projects/<str:pk>/users/',
-        'Get the list of users on a project(GET)': '/projects/<str:pk>/users/',
+        'Get the list of users of a project(GET)': '/projects/<str:pk>/users/',
         'Delete a user from a project (DELETE)': '/projects/<str:pk>/users/<str:pk>',
-        'Get the list of issues on a project (GET)': '/projects/<str:pk>/issues/',
-        'Create an issue on a project (POST)': '/projects/<str:pk>/issues/',
-        'Update an issue on a project (PUT)': '/projects/<str:pk>/issues/<str:pk>',
-        'Delete an issue on a project (DELETE)': '/projects/<str:pk>/issues/<str:pk>',
+        'Get the list of issues for a project (GET)': '/projects/<str:pk>/issues/',
+        'Create an issue for a project (POST)': '/projects/<str:pk>/issues/',
+        'Update an issue for a project (PUT)': '/projects/<str:pk>/issues/<str:pk>',
+        'Delete an issue for a project (DELETE)': '/projects/<str:pk>/issues/<str:pk>',
         'Create comment about an issue (POST)': '/projects/<str:pk>/issues/<str:pk>/comments/',
-        'Get the list of comments about an issue (GET)': '/projects/<str:pk>/issues/<str:pk>/comments/',
+        'Get the list of comments for an issue (GET)': '/projects/<str:pk>/issues/<str:pk>/comments/',
         'Update a comments (PUT)': '/projects/<str:pk>/issues/<str:pk>/comments/<str:pk>',
         'Delete a comments (DELETE)': '/projects/<str:pk>/issues/<str:pk>/comments/<str:pk>',
         'Get a comments with its id (GET)': '/projects/<str:pk>/issues/<str:pk>/comments/<str:pk>',
     }
     return Response(api_urls)
 
+
 @api_view(['GET'])
-def project_list(request):
+def get_projects_list(request):
     projects = Project.objects.all()
     serializer = ProjectSerializer(projects, many=True)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def create_project(request):
+    pass
+
+
+@api_view(['GET'])
+def get_project_details(request):
+    pass
+
+
+@api_view(['PUT'])
+def update_project(request):
+    pass
+
+
+@api_view(['DELETE'])
+def delete_project(request):
+    pass
+
+
+@api_view(['POST'])
+def add_user_to_project(request):
+    pass
+
+
+@api_view(['GET'])
+def get_users_list_on_project(request):
+    pass
+
+
+@api_view(['POST'])
+def create_project(request):
+    pass
+
+
+@api_view(['DELETE'])
+def delete_user_from_project(request):
+    pass
+
+
+@api_view(['GET'])
+def get_issues_list_for_project(request):
+    pass
+
+
+@api_view(['POST'])
+def create_issue_for_project(request):
+    pass
+
+
+@api_view(['PUT'])
+def update_issue_project(request):
+    pass
+
+
+@api_view(['DELETE'])
+def delete_issue_project(request):
+    pass
+
+
+@api_view(['POST'])
+def create_comment_for_issue(request):
+    pass
+
+
+@api_view(['GET'])
+def get_comments_list_for_issue(request):
+    pass
+
+
+@api_view(['POST'])
+def create_issue_for_project(request):
+    pass
+
+
+@api_view(['PUT'])
+def update_comment(request):
+    pass
+
+
+@api_view(['DELETE'])
+def delete_comment(request):
+    pass
+
+
+@api_view(['GET'])
+def get_comment_by_id(request):
+    pass
