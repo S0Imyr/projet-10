115c3de1b43e2a05e941f30ea4757d6a5fe2e8e3
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-04-17 11:40:58 +0200

    migrate to postgresql

--- projet10/projet10/settings.py
+++ projet10/projet10/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'projet10.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'projet10db',
+        'USER': 'Raphael',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
 
