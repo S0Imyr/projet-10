9af45c17fb564f496c352debcdcace2ffc41fd27
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-04-22 20:29:23 +0200

    logout

--- projet10/authentication/views.py
+++ projet10/authentication/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
