7239d72ed68c3e52d4dd4094d7eaf951a7a80db7
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-04-22 21:15:41 +0200

    __str__ methods for models

--- projet10/api/models.py
+++ projet10/api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 
 
-CHOICES = ((1,'not allowed'), (2,'allowed'))
+CHOICES = (('not allowed','not allowed'), ('allowed','allowed'))
 
 
 class Project(models.Model):
@@ -10,6 +10,8 @@ class Project(models.Model):
     description = models.CharField(max_length=8192)
     type = models.CharField(max_length=128)
     author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
+    def __str__(self):
+        return self.title
 
 
 class Contributor(models.Model):
@@ -17,6 +19,8 @@ class Contributor(models.Model):
     project_id = models.ForeignKey(to=Project, on_delete=models.CASCADE)
     permission = models.CharField(max_length=128, choices=CHOICES)
     role = models.CharField(max_length=128)
+    def __str__(self):
+        return f"{self.user_id} work on {self.project_id}"
 
 
 class Issue(models.Model):
@@ -29,6 +33,8 @@ class Issue(models.Model):
     author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL, related_name="Written_by")
     assignee_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL, related_name="Assign_to")
     created_time = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"Project: {self.project_id} - {self.title}"
 
 
 class Comment(models.Model):
@@ -36,3 +42,5 @@ class Comment(models.Model):
     author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
     issue_id = models.ForeignKey(to=Issue, on_delete=models.CASCADE)
     created_time = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"Issue {self.issue_id} - comment {self.id}"
