387296d1b24889bbf96a206e534f9699268565f9
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-05-06 15:37:53 +0200

    permission in module

---
+++ projet10/api/permissions.py
@@ -0,0 +1,36 @@
+from rest_framework.permissions import BasePermission, IsAdminUser, IsAuthenticated
+
+
+class IsContributor(BasePermission):
+    message = 'Only contributors are allowed'
+
+    def has_permission(self, request, view):
+        contributors = []
+        if 'pk' in view.kwargs:
+            project = get_object_or_404(Project, pk=view.kwargs['pk'])
+        elif 'pk1' in view.kwargs:
+            project = get_object_or_404(Project, pk=view.kwargs['pk1'])
+        contributions = Contributor.objects.filter(project_id=project)
+        for contribution in contributions:
+            contributors.append(contribution.user_id)
+        return  request.user in contributors
+        
+
+    def has_object_permission(self, request, view, obj):
+        if isinstance(obj, Project):
+            contributors = []
+            contributions = Contributor.objects.filter(project_id=obj)
+            for contribution in contributions:
+                contributors.append(contribution.user_id)
+            return  request.user in contributors
+        else:
+            return False
+
+class IsAuthor(BasePermission):
+    message = 'Only the author is allowed'
+
+    def has_permission(self, request, view):
+        pass
+
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.author_user_id
--- projet10/api/views.py
+++ projet10/api/views.py
@@ -6,11 +6,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from rest_framework.permissions import BasePermission, IsAdminUser, IsAuthenticated
-
 from.serializers import ProjectSerializer, IssueSerializer, CommentSerializer, ContributorSerializer
 from .models import Project, Issue, Comment, Contributor
 from authentication.models import User
+
+from .permissions import IsContributor, IsIssueAuthor, IsCommentAuthor
 
 
 @api_view(['GET'])
@@ -27,41 +27,6 @@ def apiOverview(request):
         'Get a comments with its id (GET)': '/projects/<str:pk>/issues/<str:pk>/comments/<str:pk>',
     }
     return Response(api_urls)
-
-
-class IsContributor(BasePermission):
-    message = 'Only contributors are allowed'
-
-    def has_permission(self, request, view):
-        contributors = []
-        if 'pk' in view.kwargs:
-            project = get_object_or_404(Project, pk=view.kwargs['pk'])
-        elif 'pk1' in view.kwargs:
-            project = get_object_or_404(Project, pk=view.kwargs['pk1'])
-        contributions = Contributor.objects.filter(project_id=project)
-        for contribution in contributions:
-            contributors.append(contribution.user_id)
-        return  request.user in contributors
-        
-
-    def has_object_permission(self, request, view, obj):
-        if isinstance(obj, Project):
-            contributors = []
-            contributions = Contributor.objects.filter(project_id=obj)
-            for contribution in contributions:
-                contributors.append(contribution.user_id)
-            return  request.user in contributors
-        else:
-            return False
-
-class IsAuthor(BasePermission):
-    message = 'Only the author is allowed'
-
-    def has_permission(self, request, view):
-        pass
-
-    def has_object_permission(self, request, view, obj):
-        return request.user == obj.author_user_id
 
 
 class ProjectsList(APIView):
