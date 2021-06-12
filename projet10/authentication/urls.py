66121cd9458f2daf7f60f1a325e4ca4cda14df29
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-06-12 23:20:42 +0200

    Suppression of logout and api-auth

--- projet10/authentication/urls.py
+++ projet10/authentication/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.Register.as_view(), name='register'),
+    # Utils
+    path('users/', views.UsersList.as_view(), name='users-list'),
 ]
+
+
+
+    
+    
--- projet10/projet10/urls.py
+++ projet10/projet10/urls.py
@@ -6,20 +6,12 @@ from django.urls import include, path
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
-from authentication import views
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('api/', include("api.urls")),
     path('api/', include("authentication.urls")),
-    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
-    # Utils
-    path('api/users', views.UsersList.as_view(), name='users-list'),
-
-    # Temp
-    path('api/logout/', views.Logout.as_view(), name='logout'),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api/login/', TokenObtainPairView.as_view(), name='login'),
+    path('api/login/refresh/', TokenRefreshView.as_view(), name='refresh'),
 ]
